4cbe958b49a31411669134408f4883898580ab9b
Pau Rafel Díaz Hernández <paurafel.diaz@enti.cat>
2025-12-05 22:36:40 +0100

    RenderManager

---
+++ source/RenderManager.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <map>
+#include <string>
+#include <cassert>
+
+#define RM RenderManager::Instance()
+
+class RenderManager
+{
+public:
+    static RenderManager& Instance()
+    {
+        static RenderManager instance;
+        return instance;
+    }
+
+    const unsigned int WINDOW_WIDTH = 1360;
+    const unsigned int WINDOW_HEIGHT = 768;
+
+    void Init();
+    void Release();
+    void ClearScreen();
+    void RenderScreen();
+
+    SDL_Renderer* GetRenderer() { return _renderer; }
+    void LoadTexture(std::string path);
+    SDL_Texture* GetTexture(std::string path);
+
+private:
+    RenderManager() = default;
+    RenderManager(RenderManager&) = delete;
+    RenderManager& operator=(const RenderManager&) = delete;
+    ~RenderManager();
+
+    SDL_Window* _window = nullptr;
+    SDL_Renderer* _renderer = nullptr;
+    std::map<std::string, SDL_Texture*> _textures;
+
+    void InitSDL();
+    void CreateWindowAndRenderer();
+};
