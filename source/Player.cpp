76360eaa5b07d876562fe40a394a9350d8564f8f
Pau Rafel Díaz Hernández <paurafel.diaz@enti.cat>
2025-12-06 16:31:31 +0100

    Player update

--- source/Player.cpp
+++ source/Player.cpp
@@ -17,6 +17,7 @@ Player::Player(std::string texturePath, Vector2 sourceOffset, Vector2 sourceSize
 void Player::Update(float dt)
 {
     Vector2 direction(0.f, 0.f);
+    Vector2 previousPosition = _transform->position;
 
     if (IM.GetEvent(SDLK_W, HOLD) || IM.GetEvent(SDLK_UP, HOLD))
         direction.y -= 1.0f;
@@ -50,12 +51,104 @@ void Player::Update(float dt)
     if (_transform->position.y > RM.WINDOW_HEIGHT)
         _transform->position.y = RM.WINDOW_HEIGHT;
 
+    float distanceX = _transform->position.x - previousPosition.x;
+    _distanceTraveled += distanceX;
+
+    if (_hasTurrets)
+    {
+        while (_distanceTraveled >= 100.0f)
+        {
+            _turretAngle -= 45.0f;
+            _distanceTraveled -= 100.0f;
+        }
+        while (_distanceTraveled <= -100.0f)
+        {
+            _turretAngle += 45.0f;
+            _distanceTraveled += 100.0f;
+        }
+    }
+
     Object::Update(dt);
 }
 
 void Player::Shoot()
 {
-    Bullet* bullet = new Bullet("resources/bullet.png", Vector2(0.f, 0.f), Vector2(32.f, 32.f));
-    bullet->GetTransform()->position = _transform->position;
-    _bulletsVector->push_back(bullet);
+    Bullet* mainBullet = new Bullet("resources/bullet.png", Vector2(0.f, 0.f), Vector2(32.f, 32.f));
+    mainBullet->GetTransform()->position = _transform->position;
+    _bulletsVector->push_back(mainBullet);
+
+    if ((_currentState == STATE_CA1 || _currentState >= STATE_CA2) && _cannonAmmo > 0)
+    {
+        Bullet* cannonBullet1 = new Bullet("resources/bullet.png", Vector2(0.f, 0.f), Vector2(32.f, 32.f));
+        cannonBullet1->GetTransform()->position = _transform->position + Vector2(0.f, 20.f);
+        _bulletsVector->push_back(cannonBullet1);
+
+        Bullet* cannonBullet2 = new Bullet("resources/bullet.png", Vector2(0.f, 0.f), Vector2(32.f, 32.f));
+        cannonBullet2->GetTransform()->position = _transform->position + Vector2(0.f, -20.f);
+        _bulletsVector->push_back(cannonBullet2);
+
+        _cannonAmmo--;
+    }
+
+    if ((_currentState == STATE_LA1 || _currentState >= STATE_LA2) && _laserAmmo > 0)
+    {
+        Bullet* laserBullet = new Bullet("resources/bullet.png", Vector2(0.f, 0.f), Vector2(32.f, 32.f));
+        laserBullet->GetTransform()->position = _transform->position + Vector2(10.f, 0.f);
+        laserBullet->GetTransform()->scale = Vector2(1.5f, 1.5f);
+        _bulletsVector->push_back(laserBullet);
+
+        _laserAmmo--;
+    }
+
+    if (_hasTurrets)
+    {
+        Bullet* turretBullet = new Bullet("resources/bullet.png", Vector2(0.f, 0.f), Vector2(32.f, 32.f));
+        turretBullet->GetTransform()->position = _transform->position;
+        _bulletsVector->push_back(turretBullet);
+    }
+}
+
+void Player::ApplyPowerUp(PowerUpState newState)
+{
+    switch (newState)
+    {
+    case STATE_NONE:
+        _score += 1000;
+        break;
+    case STATE_CA1:
+        _cannonAmmo = 50;
+        _currentState = STATE_CA1;
+        break;
+    case STATE_LA1:
+        _laserAmmo = 30;
+        _currentState = STATE_LA1;
+        break;
+    case STATE_SPEED:
+        _moveSpeed = _baseSpeed * 1.5f;
+        _currentState = STATE_SPEED;
+        break;
+    case STATE_CA2:
+        _cannonAmmo = 100;
+        _currentState = STATE_CA2;
+        break;
+    case STATE_LA2:
+        _laserAmmo = 60;
+        _currentState = STATE_LA2;
+        break;
+    case STATE_SHIELD:
+        _shield = 100; 
+        _currentState = STATE_SHIELD;
+        break;
+    case STATE_TURRETS:
+        _hasTurrets = true;
+        _currentState = STATE_TURRETS;
+        break;
+    }
+}
+
+void Player::TakeDamage(int damage)
+{
+    _shield -= damage;
+    if (_shield < 0)
+        _shield = 0;
 }
--- source/Player.h
+++ source/Player.h
@@ -6,15 +6,45 @@
 
 class Bullet;
 
+enum PowerUpState
+{
+    STATE_NONE,
+    STATE_CA1,      
+    STATE_LA1,     
+    STATE_SPEED, 
+    STATE_CA2,     
+    STATE_LA2,     
+    STATE_SHIELD,   
+    STATE_TURRETS  
+};
+
 class Player : public ImageObject
 {
 private:
     float _moveSpeed = 300.0f;
+    float _baseSpeed = 300.0f;
     std::vector<Bullet*>* _bulletsVector;
+
+    PowerUpState _currentState = STATE_NONE;
+    bool _hasTurrets = false;
+    int _cannonAmmo = 0;    
+    int _laserAmmo = 0;    
+    int _shield = 100;  
+    int _score = 0;
+
+    float _distanceTraveled = 0.0f;
+    float _turretAngle = 0.0f;  
 
 public:
     Player(std::string texturePath, Vector2 sourceOffset, Vector2 sourceSize, std::vector<Bullet*>* bulletsVector);
 
     void Update(float dt) override;
     void Shoot();
+
+    void ApplyPowerUp(PowerUpState newState);
+    PowerUpState GetCurrentState() const { return _currentState; }
+    int GetScore() const { return _score; }
+    void AddScore(int amount) { _score += amount; }
+    int GetShield() const { return _shield; }
+    void TakeDamage(int damage);
 };
