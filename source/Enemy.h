faf5bd1c8f9bb0364d96ad65d34bac74c94717f4
Pau Rafel Díaz Hernández <paurafel.diaz@enti.cat>
2025-12-08 23:02:13 +0100

    Enemy update

--- source/Enemy.h
+++ source/Enemy.h
@@ -2,18 +2,23 @@
 #include "ImageObject.h"
 #include "RenderManager.h"
 #include "TimeManager.h"
+#include "EnemyStateMachine.h"
 #include <cmath>
 
 class Enemy : public ImageObject
 {
-private:
-    Vector2 _centerPoint;  
-    float _radius;        
-    float _angle;          
-    float _angularSpeed;   
+protected:
+    EnemyStateMachine* _stateMachine;
+    int _health;
+    int _scoreValue;
 
 public:
     Enemy(std::string texturePath, Vector2 sourceOffset, Vector2 sourceSize, Vector2 spawnPosition);
-
+    virtual ~Enemy();
     void Update(float dt) override;
+    void TakeDamage(int damage);
+    int GetHealth() const { return _health; }
+    int GetScoreValue() const { return _scoreValue; }
+
+    EnemyStateMachine* GetStateMachine() { return _stateMachine; }
 };
