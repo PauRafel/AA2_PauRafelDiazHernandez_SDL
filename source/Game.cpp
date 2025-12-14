a5b1e4211c2326ca8ef11433896a742d976225df
Pau Rafel Díaz Hernández <paurafel.diaz@enti.cat>
2025-12-14 11:35:46 +0100

    Game update
    BubbleEnemy sprite loaded

--- source/Game.cpp
+++ source/Game.cpp
@@ -13,7 +13,8 @@ void Game::Init()
     RM.LoadTexture("resources/bullet.png");
     RM.LoadTexture("resources/enemy.png");
     RM.LoadTexture("resources/background.png");
-    RM.LoadTexture("resources/powerup.png"); 
+    RM.LoadTexture("resources/powerup.png");
+    RM.LoadTexture("resources/BubbleEnemy.png");
 
     assert(SM.AddScene("MainMenu", new MainMenu()));
     assert(SM.AddScene("Gameplay", new Gameplay()));
